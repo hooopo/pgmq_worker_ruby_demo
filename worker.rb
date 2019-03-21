a4f9707dc1f832cfa23191b87cbd6b0116bc6633
Hooopo Wang <hoooopo@gmail.com>
2019-03-21 23:06:08 +0800

    on conflict update

--- worker.rb
+++ worker.rb
@@ -30,7 +30,17 @@ labels         = '{ruby, pgmq_worker_ruby_demo}'
 started_at     = Time.now.utc
 last_active_at = Time.now.utc
 
-db.prepare("create_worker", "insert into workers (hostname, pid, v, labels, started_at, last_active_at) values ($1, $2, $3, $4, $5, $6) returning *")
+db.prepare("create_worker", %Q{
+  insert into workers (hostname, pid, v, labels, started_at, last_active_at) 
+       values ($1, $2, $3, $4, $5, $6) 
+  on conflict (pid)
+              do update set last_active_at = EXCLUDED.last_active_at, 
+                            hostname = EXCLUDED.hostname, 
+                            v = EXCLUDED.v,
+                            labels = EXCLUDED.labels,
+                            started_at = EXCLUDED.started_at
+    returning *
+})
 result = db.exec_prepared("create_worker", [hostname, pid, v, labels, started_at, last_active_at])[0]
 current_worker = Worker.new(result)
 
